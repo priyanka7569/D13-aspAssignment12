bc14e5a8eb61d011618e99d5ba36829d8130cde6
priyanka7569 <kota.priyanka@mphasis.com>
2023-12-27 08:44:35 +0530

    initial commit

---
+++ Assignment12/Assignment12/Controllers/TeamController.cs
@@ -0,0 +1,24 @@
+﻿using Assignment12.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Assignment12.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        static List<Team> ListTeams = new List<Team>()
+        {
+            new Team(){TeamId=1,TeamName="India",NOWC=3},
+            new Team(){TeamId=2,TeamName="Australia",NOWC=4},
+            new Team(){TeamId=3,TeamName="West Indies",NOWC=2},
+            new Team(){TeamId=4,TeamName="England",NOWC=1}
+        };
+
+        public ActionResult Index()
+        {
+
+            return View(ListTeams);
+        }
+    }
+}
